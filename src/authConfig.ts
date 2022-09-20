112d492796f586bab0d8f3c85d3dfcf18cda2ece
ko <ko@mqa.com>
2022-09-20 17:34:41 +0700

    update config authen

--- src/authConfig.ts
+++ src/authConfig.ts
@@ -3,22 +3,22 @@ import { LogLevel } from "@azure/msal-browser";
 
 export const b2cPolicies = {
   names: {
-    signUpSignIn: "B2C_1_signupsignin",
-    forgotPassword: "B2C_1_reset_password",
-    editProfile: "B2C_1_profileediting",
+    signUpSignIn: "B2C_1_signin_signup_openid",
+    forgotPassword: "B2C_1_resetpw_openid",
+    editProfile: "B2C_1_profile_edit_openid",
   },
   authorities: {
     signUpSignIn: {
       authority:
-        "https://mapquestcorp.b2clogin.com/mapquestcorp.onmicrosoft.com/B2C_1_signupsignin",
+        "https://mapquestcorp.b2clogin.com/mapquestcorp.onmicrosoft.com/B2C_1_signin_signup_openid",
     },
     forgotPassword: {
       authority:
-        "https://mapquestcorp.b2clogin.com/mapquestcorp.onmicrosoft.com/B2C_1_reset_password",
+        "https://mapquestcorp.b2clogin.com/mapquestcorp.onmicrosoft.com/B2C_1_resetpw_openid",
     },
     editProfile: {
       authority:
-        "https://mapquestcorp.b2clogin.com/mapquestcorp.onmicrosoft.com/B2C_1_profileediting",
+        "https://mapquestcorp.b2clogin.com/mapquestcorp.onmicrosoft.com/B2C_1_profile_edit_openid",
     },
   },
   authorityDomain: "mapquestcorp.b2clogin.com",
@@ -26,7 +26,7 @@ export const b2cPolicies = {
 
 export const msalConfig = {
   auth: {
-    clientId: "bfca7d80-3491-485c-9b36-f5011edc585b",
+    clientId: "af9bca26-389b-4196-93b8-dc55080188e5",
     authority: b2cPolicies.authorities.signUpSignIn.authority,
     knownAuthorities: [b2cPolicies.authorityDomain],
     navigateToLoginRequestUrl: true,
